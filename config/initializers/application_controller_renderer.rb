4b6ec2007228ce6dc0fc9f0fda10fbf9e5e616e8
mkkoala <mako1105.96@gmail.com>
2019-12-11 19:49:23 +0900

    [Update] 5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '998ae7b63de72e76da6eac9859553d663217f9a37bef5dda1e89a4d1de4fa86a0b682968e0cf0f28206d827caeb74c04b61feb99037f8059e8d557513e0732a1'
